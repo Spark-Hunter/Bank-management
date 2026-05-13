63cd6c2c1147c77625909fc339333a52d88922e6
Mahadi-Maharuf17 <maharuf69@gmail.com>
2026-05-14 01:06:24 +0600

    edit

--- Person.java
+++ Person.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 class Person {
     // Attributes
     private String personId;
@@ -41,50 +39,5 @@ class Person {
         this.address = address;
     }
 
-    // Main method
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-
-        // Taking input from user
-        System.out.print("Enter Person ID: ");
-        String id = input.nextLine();
-
-        System.out.print("Enter Name: ");
-        String name = input.nextLine();
-
-        System.out.print("Enter Phone: ");
-        String phone = input.nextLine();
-
-        System.out.print("Enter Email: ");
-        String email = input.nextLine();
-
-        System.out.print("Enter Address: ");
-        String address = input.nextLine();
-
-        // Creating object
-        Person p1 = new Person(id, name, phone, email, address);
-
-        // Display information
-        p1.getpersonInfo();
-
-        // Updating contact info
-        System.out.println("\nUpdate Contact Information");
-
-        System.out.print("Enter New Phone: ");
-        String newPhone = input.nextLine();
-
-        System.out.print("Enter New Email: ");
-        String newEmail = input.nextLine();
-
-        System.out.print("Enter New Address: ");
-        String newAddress = input.nextLine();
-
-        // Update values
-        p1.updateContact(newPhone, newEmail, newAddress);
-
-        // Display updated information
-        p1.getpersonInfo();
-
-    }
+   
 }
